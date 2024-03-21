633292770fb1bbef7a0c7f93bf0d37b5b5e8a0d4
gnomagin Dallocort <email@email.com>
2024-03-21 19:08:55 -0400

    Seperated functions into theire own functions seperated reused commands of finding ip part and finding mask part into theire own functions. started to add function to find submask id and size based on number of hosts

--- NetworkCalculator.py
+++ NetworkCalculator.py
@@ -1,24 +1,43 @@
 import math
 import sys
 
-print("enter ip and submask in format ***.***.***.***/**")
 
-a = input()
+def ipOctets(a: str):
+     
+    a = a.split("/")
 
-print("enter number of subnets to split into")
+    if(a[0].count(".") != 3):
+        sys.stderr("[Input Error] - Invalid IP adresse\n")
+        sys.exit(1)
 
-b = input()
+    b = a[0].split(".")
 
-a = a.split("/")
-mask = a[1]
+    return b
 
-if(a[0].count(".") > 3):
-    print("INVALID IP OR SUBNET MASK")
-else:
+def ipMask(a: str):
+    print(a)
+    print("mask count: " + str(a.count("/")))
+    if(a.count("/") != 1):
+        sys.stderr("[Input Error] - Invalid IP MASK\n")
+        sys.exit(1)
+    a = a.split("/")
+    b = a[1]
+    print(b)
 
-    fullip = a[0].split(".")
+    return b
+
+
+def subMaskSplit(a: str):
+    print("enter number of subnets to split into")
+
+    b = input()
+
+
+    mask = ipMask(a)
+    fullip = ipOctets(a)
     maskoctet = math.floor(int(mask) / 8)
     maskoctetpos = int(mask) % 8
+
 
     #this is trigerd if the mask is a multiple of 8
     x = 0
@@ -43,8 +62,9 @@ else:
 
     #checks for possible errors should never happen
     if(subnet - int(subnet) != 0):
-            sys.exit("[Program Error] - non whole value detected")
-    
+            sys.stderr("[Program Error] - non whole value detected\n")
+            sys.exit(1)
+
     subnet = int(subnet) + int(fullip[maskoctet])
     x = 0
     finalanswer = str()
@@ -64,6 +84,58 @@ else:
     print("===================================================")
     
 
+def submaskSize(a: str):
+     
+    a = a.split("/")    
+    mask = a[1]
+    b = input("how many hosts per network do you need (put ',' between values for more then one): ")
 
+    b = b.split(",")
+    b = sorted(b, reverse=True)
+    subnetsize = []
+    for hostCount in b:
+        bitsize = 0
+        hostsize = 0
+        while(hostsize + 2 < int(hostCount)):
+            hostsize = math.pow(2, bitsize)
+            bitsize = bitsize + 1
+            decnum = math.pow(2, bitsize)
+            print(str(bitsize) + ' - ' + str(int(hostsize)))
+            
+
+        if(bitsize > int(mask)):
+             sys.stderr("[Process Error] host size to large for given network\n")
+             sys.exit(1)
+        x = 0
+        subnetsize.append(bitsize)
+    x = 0
+    print("===================")
+    while(len(b) > x):
+
+
+         print(str(b[x]) + " - " + str(subnetsize[x]))
+         x = x + 1
+
+             
+          
+          
 
     
+
+
+
+
+
+print("enter current ip and submask in format ***.***.***.***/**")
+
+a = input("ip: ")
+
+print("choose wich function you would like to perform")
+print("1 - subnet start calculator")
+print("2 - get size of subnets for given host count")
+function = input()
+
+if(function == "1"):
+     subMaskSplit(str(a))
+elif(function == "2"):
+     submaskSize(str(a))
